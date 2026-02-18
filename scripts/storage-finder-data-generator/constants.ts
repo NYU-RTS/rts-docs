7d91a7bd8b426b6b1e7d31c5317f5c268208b2f1
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-02-18 11:56:25 -0500

    update source google sheet URL and regenerate data

--- scripts/storage-finder-data-generator/constants.ts
+++ scripts/storage-finder-data-generator/constants.ts
@@ -1,7 +1,7 @@
 export const STORAGE_FINDER_ENV_URL_KEY = "STORAGE_FINDER_SHEET_URL";
 
 export const DEFAULT_STORAGE_FINDER_SHEET_URL =
-  "https://docs.google.com/spreadsheets/d/12vxBpVUpWTrPmZ-3e30IbyyoAG9qQ6ULpMq5O8okPtA/export?format=csv&gid=1073279644";
+  "https://docs.google.com/spreadsheets/d/1-e_Xf8731aODXmn83IZfo-fXNWutd-NaUqKmM88QIk0/export?format=csv&gid=1073279644";
 
 export const OUTPUT_DIRECTORY = "src/data/storage-finder/generated";
 
